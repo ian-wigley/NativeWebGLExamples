46c432bd7cf6656c90fef9a88b075d1daa7dba0e
Ian Wigley <ian.wigley@outlook.com>
2023-08-31 18:34:45 +0100

    Update Camera.ts

--- Tut07-VSCode - Camera/Camera.ts
+++ Tut07-VSCode - Camera/Camera.ts
@@ -1,18 +1,24 @@
 import * as mat4 from "./glmatrix/mat4.js"
+import * as vec3 from "./glmatrix/vec3.js"
 
 export class Camera {
 
-    public x: number = 0;
-    public y: number = 0;
-    public z: number = 0;
+    public x: number = 1;
+    public y: number = 1;
+    public z: number = 2;
 
     private zNear: number = 0.1;
     private zFar: number = 100.0;
     private aspect: number = 0;
+    private movement_speed: number = 6;
     // In radians
     private fieldOfView: number = 45 * Math.PI / 180;
     private gl: WebGLRenderingContext;
-    private pMatrix = mat4.create();
+    private projectionMatrix = mat4.create();
+
+    private position = vec3.fromValues(this.x, this.y, this.z);
+    private world_up = vec3.fromValues(0, 1, 0);
+    private front = vec3.fromValues(0, 0, -1);
 
     constructor(gl: WebGLRenderingContext) {
         this.gl = gl;
@@ -20,21 +26,33 @@ export class Camera {
 
     public Initialise(): object {
         this.aspect = this.gl.drawingBufferWidth / this.gl.drawingBufferHeight;
-        mat4.perspective(this.pMatrix,
+        mat4.perspective(this.projectionMatrix,
             this.fieldOfView,
             this.aspect,
             this.zNear,
             this.zFar);
-        return this.pMatrix;
+        return this.projectionMatrix;
     }
 
-    public MoveForward(amount: number) {
-        this.z = amount;
+    public MoveForward(value: number): void {
+        this.z = value;
+        let velocity = this.movement_speed * value
+        this.position.z += velocity;
     }
 
-    public Update() {
+    public Update(): void {
         this.x = 0;
         this.y = 0;
         this.z = 0;
     }
+
+    public get_view_matrix(): object {
+        let viewMatrix = mat4.create();
+        return mat4.lookAt(viewMatrix, this.position, this.position + this.front, this.world_up);
+    }
+
+    public get_projection_matrix(): object {
+        return this.projectionMatrix;
+    }
+
 }
